407bd53526e5dcbf8c9f58fd9bb5963f49f480f6
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-16 10:51:51 -0400

    ejercicio listo

---
+++ main.js
@@ -0,0 +1,11 @@
+function carga(){
+	alert("La página está completamente cargada!");
+}
+
+function mouseout(){
+	alert("Estas sobre mí");
+}
+
+function mouseover(){
+	alert("No estás sobre mí");	
+}
